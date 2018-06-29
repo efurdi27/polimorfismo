e131a1ff102f62033845638a6a786aa7417ad8f0
efurdi27 <36687055+efurdi27@users.noreply.github.com>
2018-06-29 16:25:29 -0500

    Create Masajista.java

---
+++ Masajista.java
@@ -0,0 +1,15 @@
+public class Masajsita extends SeleccionFutbol{
+
+   private String titulacion;
+  private int aniosExperiencia;
+
+  //constructor, getter y setter
+  @Override
+  public void entrenamiento() {
+  System.out.println("Da asistencia en el entrenamiento (Clase Masajista)");
+  }
+  public void darMasaje(){
+  System.out.println("Da un Masaje");
+  }
+}
+
